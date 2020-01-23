d12613dd1e06e7e69f7dc43424c4e1281b28c22e
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-01-22 19:15:50 -0800

    Basic HTML and CSS framework completed.

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(alienNavigation).on("click", function() {
+//   var alienNavigation = $("#navigation-widget");
+
+//   if (alienNavigation.style.display === "none") {
+//     alienNavigation.style.display = "block";
+//   } else {
+//     alienNavigation.style.display = "none";
+//   }
+// });
