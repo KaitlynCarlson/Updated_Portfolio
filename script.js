ede922889ee6396c657e8cf460c1578f3d238bfe
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-20 15:07:15 -0700

    Added note taker application

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
 $("#githubrepo").on("click", function() {
-  window.location = "https://github.com/KaitlynCarlson";
+  window.open("https://github.com/KaitlynCarlson");
 });
 $("#linkedin").on("click", function() {
-  window.location = "https://www.linkedin.com/in/kaitlynannecarlson";
+  window.open("https://www.linkedin.com/in/kaitlynannecarlson");
 });
 $("#email").on("click", function() {
   $(".emaildropdowncontent").css("display", "block");
   $(".phonedropdowncontent").css("display", "none");
 });
 $("#chooseyourownadventurerepo").on("click", function() {
-  window.location = "https://github.com/KaitlynCarlson/Project-One";
+  window.open("https://github.com/KaitlynCarlson/Travel-Application");
 });
 $("#weatherrepo").on("click", function() {
-  window.location = "https://github.com/KaitlynCarlson/Weather-Dashboard";
+  window.open("https://github.com/KaitlynCarlson/Weather-Dashboard");
 });
 
 $("#dayplannerrepo").on("click", function() {
-  window.location = "https://github.com/KaitlynCarlson/Work-Day-Planner";
+  window.open("https://github.com/KaitlynCarlson/Work-Day-Planner");
 });
 $("#randompasswordgeneratorrepo").on("click", function() {
-  window.location = "https://github.com/KaitlynCarlson/PasswordGenerator";
+  window.open("https://github.com/KaitlynCarlson/PasswordGenerator");
 });
 $("#phone").on("click", function() {
   $(".phonedropdowncontent").css("display", "block");
   $(".emaildropdowncontent").css("display", "none");
 });
 $("#adventure").on("click", function() {
-  window.location = "https://kaitlyncarlson.github.io/Project-One/";
+  window.open("https://kaitlyncarlson.github.io/Travel-Application/");
 });
 $("#weather").on("click", function() {
-  window.location = "https://kaitlyncarlson.github.io/Weather-Dashboard/";
+  window.open("https://kaitlyncarlson.github.io/Weather-Dashboard/");
 });
 $("#dayplanner").on("click", function() {
-  window.location = "https://kaitlyncarlson.github.io/Work-Day-Planner/";
+  window.open("https://kaitlyncarlson.github.io/Work-Day-Planner/");
 });
 $("#randompassword").on("click", function() {
-  window.location = "https://kaitlyncarlson.github.io/PasswordGenerator/";
+  window.open("https://kaitlyncarlson.github.io/PasswordGenerator/");
 });
 $("#burgerImg").on("click", function() {
-  window.location = "https://rocky-dusk-41654.herokuapp.com/";
+  window.open("https://rocky-dusk-41654.herokuapp.com/");
 });
 $("#buger").on("click", function() {
-  window.location = "https://github.com/KaitlynCarlson/burger";
+  window.open("https://github.com/KaitlynCarlson/burger");
 });
 $("#mothraImg").on("click", function() {
-  window.location = "https://the-best-project-manager.herokuapp.com/";
+  window.open("https://the-best-project-manager.herokuapp.com/");
 });
 $("#mothra").on("click", function() {
-  window.location =
-    "https://github.com/maximilianc88/Project-Planner-Application";
+  window.open("https://github.com/maximilianc88/Project-Planner-Application");
+});
+$("#noteTakerImg").on("click", function() {
+  window.open("https://note--taker.herokuapp.com/");
+});
+$("#noteTaker").on("click", function() {
+  window.open("https://github.com/KaitlynCarlson/Note-Taker");
 });
 AOS.init({
   duration: 1000
